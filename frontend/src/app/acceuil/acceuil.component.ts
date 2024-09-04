706cb7c4208a6e72221a97289d663cf35c9abdb9
tim <timdesmet00@gmail.com>
2024-09-04 09:11:57 +0200

    integration de la nav bar et du  normalize

--- frontend/src/app/acceuil/acceuil.component.ts
+++ frontend/src/app/acceuil/acceuil.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from "../navbar/navbar.component";
 
 @Component({
   selector: 'app-acceuil',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './acceuil.component.html',
   styleUrl: './acceuil.component.scss'
 })
