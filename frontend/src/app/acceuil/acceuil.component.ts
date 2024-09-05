a49e13f4f1c36c92360b4ab8a0258939369b4a2b
tim <timdesmet00@gmail.com>
2024-09-05 08:37:32 +0200

    mise en place d'un composant header en dehors de la page acceuil

--- frontend/src/app/acceuil/acceuil.component.ts
+++ frontend/src/app/acceuil/acceuil.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../navbar/navbar.component";
 import { FooterComponent } from "../footer/footer.component";
+import { HeaderComponent } from "../header/header.component";
 
 @Component({
   selector: 'app-acceuil',
   standalone: true,
-  imports: [NavbarComponent, FooterComponent],
+  imports: [NavbarComponent, FooterComponent, HeaderComponent],
   templateUrl: './acceuil.component.html',
   styleUrl: './acceuil.component.scss'
 })
